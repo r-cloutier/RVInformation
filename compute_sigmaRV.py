789e78aa02cdbed28935a19c830defcc213fdde6
Ryan Cloutier <cloutier@astro.utoronto.ca>
2017-09-22 12:18:38 -0400

    still working on calculating sigmaRV from the quality factor

--- compute_sigmaRV.py
+++ compute_sigmaRV.py
@@ -7,7 +7,7 @@ import numpy as np
 import pylab as plt
 import astropy.io.fits as fits
 from scipy.ndimage.filters import gaussian_filter1d
-from scipy.interpolate import interp1d
+from scipy.interpolate import interp1d, UnivariateSpline
 from PyAstronomy.pyasl import broadGaussFast
 
 
@@ -15,7 +15,21 @@ global c, h
 c, h = 299792458., 6.62607004e-34
 
 
-def get_wavelengthgrid():
+def get_reduced_spectrum(Teff, logg, Z, band_str, mag, texp_min,
+                         aperture_m=3.58, QE=.1, R=75e3):
+    '''
+    Download a stellar spectrum and get the reduced spectrum in the spectral 
+    bin of interest.
+    '''
+    wl = _get_wavelengthgrid()
+    _, spectrum = _get_full_spectrum(Teff, logg, Z)
+    wl_conv, spec_conv = _convolve_band_spectrum(wl, spectrum, band_str, R=R)
+    spec_scaled = _rescale_spectrum(spec_conv, mag, band_str, texp_min,
+                                    aperture_m, QE, R)
+    return wl_conv, spec_scaled
+
+
+def _get_wavelengthgrid():
     '''
     Read-in the wavelength grid for the PHOENIX model spectra and return it in
     microns.
@@ -24,7 +38,7 @@ def get_wavelengthgrid():
     return np.ascontiguousarray(dat.data) * 1e-4
 
 
-def get_spectrum(Teff, logg, Z):
+def _get_full_spectrum(Teff, logg, Z):
     '''
     Read-in model spectra from the PHOENIX library and return the header and 
     the spectral data.
@@ -37,7 +51,7 @@ def get_spectrum(Teff, logg, Z):
     Zs = np.append(np.arange(-4,-2,1), np.arange(-2,1.5,.5))
     assert Z in Zs
     
-    # Download the spectrum 
+    # Download the spectrum
     prefix = "ftp://phoenix.astro.physik.uni-goettingen.de/HiResFITS/" + \
              "PHOENIX-ACES-AGSS-COND-2011/"
     fname = "Z-%.1f/lte%.5d-%.2f-%.1f"%(Z, Teff, logg, Z) + \
@@ -45,26 +59,6 @@ def get_spectrum(Teff, logg, Z):
     spec_fits = fits.open(prefix+fname)[0]
     
     return spec_fits.header, spec_fits.data
-
-
-def _get_band_transmission(band_str):
-    '''
-    Read-in the transmission curve of a specific band on the same wavelength 
-    grid as the PHOENIX spectra.
-    '''
-    if band_str == 'J':
-        fname = '2MASS-2MASS.J.dat'
-    elif band_str == 'H':
-        fname = '2MASS-2MASS.H.dat'
-    elif band_str == 'Ks':
-        fname = '2MASS-2MASS.Ks.dat'
-    else:
-        raise ValueError('Do not know what input bandpass %s is.'%band_str)
-
-    wl, transmission = np.loadtxt('input_data/%s'%fname).T
-    wl *= 1e-4   # angstrom -> microns
-
-    return wl, transmission, np.average(wl, weights=transmission)
 
 
 def _convolve_band_spectrum(wl_microns, spectrum, band_str, R=75e3):
@@ -95,37 +89,109 @@ def _convolve_band_spectrum(wl_microns, spectrum, band_str, R=75e3):
     return wl_band, spectrum_conv
 
 
-def get_reduced_spectrum(Teff, logg, Z, band_str, R=75e3):
+def _get_band_transmission(band_str):
     '''
-    Download a stellar spectrum and get the reduced spectrum in the spectral 
-    bin of interest.
+    Read-in the transmission curve of a specific band on the same wavelength 
+    grid as the PHOENIX spectra.
     '''
-    wl = get_wavelengthgrid()
-    _, spectrum = get_spectrum(Teff, logg, Z)
-    wl_conv, spec_conv = _convolve_band_spectrum(wl, spectrum, band_str, R=R)
-    return wl_conv, spec_conv
+    if band_str == 'J':
+        fname = '2MASS-2MASS.J.dat'
+    elif band_str == 'H':
+        fname = '2MASS-2MASS.H.dat'
+    elif band_str == 'Ks':
+        fname = '2MASS-2MASS.Ks.dat'
+    else:
+        raise ValueError('Do not know what input bandpass %s is.'%band_str)
+
+    wl, transmission = np.loadtxt('input_data/%s'%fname).T
+    wl *= 1e-4   # angstrom -> microns
+
+    return wl, transmission, np.average(wl, weights=transmission)
 
 
-def _compute_Q(wl, spec):
+def _rescale_spectrum(spec, mag, band_str, texp_min, aperture_m, QE, R):
     '''
-    Compute the quality factor Q from the input spectrum using the Eqs. from 
-    Bouchy+2001.
+    Rescale the spectrum such that its sum over the band equals the SNR.
+    spec` = k*spec where k = snr^2 / sum(spec) since sum(spec) = Nphot = snr^2
     '''
+    snr = _get_snr(mag, band_str, texp_min, aperture_m, QE, R)
+    factor = snr**2 / np.sum(spec)
+    return factor * spec
+
+
+def _get_snr(mag, band, texp_min, aperture_m, QE, R):
+    '''
+    Compute the SNR of the spectrum from the apparent magnitude of
+    the star in a certain band (e.g. 'J'), the exposure time in
+    minutes, the aperture of the telescope in meters, detector efficiency,
+    the element resolution in km/s.
+    '''
+    # Define constants
+    area_cm2 = np.pi * (aperture_m*1e2/2)**2
+    res_kms = c/R
+    texp_s = texp_min * 60.
+    c_As = c * 1e10
+    c_kms = c_As * 1e-13
+    h_ergss = h * 1e7
+
+    # Get flux density zeropoint (for m=0) in ergs s^-1 A^-1 cm^-2,
+    # wavelength in angstroms and bandwidth in microns
+    if band == 'Y':
+        Fl = 6.063e-10
+        l  = 10174.
+    elif band == 'J':
+        Fl = 3.143e-10
+        l  = 12350.
+    elif band == 'H':
+        Fl = 1.144e-10
+        l  = 16620.
+    elif band == 'K':
+        Fl = 4.306e-11
+        l  = 21590.
+    else:
+        raise ValueError('Not sure which band to use')
+
+    fl = Fl * 10**(-.4 * mag)
+    Ephot = h_ergss * c_As / l
+    Nphot_A = fl * texp_s * area_cm2 * QE / Ephot
+    
+    # Get # photons per resolution element (dl/l)
+    Nphot_res = Nphot_A * (l / R)
+
+    # Add photon noise and dark current to noise budget
+    darkcurrent, footprint = 1e-2, 12     # electrons/s, pixels
+    SNR = Nphot_res / np.sqrt(Nphot_res + darkcurrent*footprint*texp_s)
+
+    return SNR
+
+
+
+###############################################################################
+###############################################################################
+def _compute_W(wl, spec):
+    '''
+    Compute the weighting function F from the input spectrum using the Eqs. 
+    from Bouchy+2001.
+    '''
+    # over-sample the spectrum
+    wl2 = np.linspace(wl.min(), wl.max(), wl.size*10)
+    fint = interp1d(wl, spec)
+    A = fint(wl2)
+
+    # compute W
+    dwl = np.diff(wl2)[0]
+    W = wl2**2 * np.gradient(A, dwl)**2 / A
+    
+    # resample W
+    fint = interp1d(wl2, W)
+    return fint(wl)
+
+
+def compute_sigmaRV(wl, spec):
     A = spec
     W = wl**2 * np.gradient(A, np.diff(wl)[0])**2 / A
-    return np.sqrt(np.sum(W)) / np.sqrt(np.sum(A))
+    g = np.arange(4, W.size-4, dtype=int)
+    return c / np.sqrt(np.sum(W[g]))
 
 
-def _compute_Nelectrons(wl_microns, spec,
-                        texp_min=5., diameter_m=3.5, QE=.1):
-    '''
-    Compute the number of photoelectrons collected.
-    '''
-    energy_ergs = h*c / (wl_microns*1e-6) * 1e7
-    texp_sec, A_cm2 = texp_min*6e1, np.pi*(diameter_m*1e2/2.)**2
-    Nphot = spec / energy_ergs * texp_sec * A_cm2 * wl_microns*1e-4
-    return Nphot * QE
-    
 
-def compute_sigmaRV():
-    return None
