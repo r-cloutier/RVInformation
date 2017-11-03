2d475b89cf35cd47f0b722572d2cd29d258a6d9a
Ryan Cloutier <cloutier@astro.utoronto.ca>
2017-11-03 14:25:34 -0400

    undergoing sigmaRV_planet testing but have a version for FGK and M stars

--- sigmaRV_planets.py
+++ sigmaRV_planets.py
@@ -3,7 +3,7 @@ import rvs
 import massradius as mr
 
 
-def draw_FGK_planet(P, rp, nplanets, Ms):
+def draw_FGK_planets(P, rp, nplanets, Ms):
     '''
     Draw planets from around FGK stars from the occurrence rates derived in 
     Fressin et al 2013. Ensure that each pair in the multi-planet system is 
@@ -24,16 +24,16 @@ def draw_FGK_planet(P, rp, nplanets, Ms):
     while Ps.size < nplanets:
         
         Ps, rps = np.array([P]), np.array([rp])
-        mps = mr.radF2mass(rps)
+        mps = mr.radF2mass(rps, Fs=np.repeat(336.5, rps.size))
         for i in range(Pgrid.size-1):
             for j in range(rpgrid.size-1):
-                
+
                 if np.random.rand() <= occurrence[i,j] and Pgrid[i] > P:
 
                     # draw a system
                     Ps  = np.append(Ps, np.random.uniform(Pgrid[i], Pgrid[i+1]))
                     rps = np.append(rps, np.random.uniform(rpgrid[j], rpgrid[j+1]))
-                    mps = mr.radF2mass(rps)
+                    mps = mr.radF2mass(rps, Fs=np.repeat(336.5, rps.size))
 
         sort = np.argsort(Ps)
         Ps, rps, mps = Ps[sort], rps[sort], mps[sort]
@@ -49,6 +49,80 @@ def draw_FGK_planet(P, rp, nplanets, Ms):
             Ps, rps, mps = Ps[lagrangestable], rps[lagrangestable], \
                            mps[lagrangestable]
 
+    # exclude TESS planet
+    Ps, rps, mps = Ps[1:], rps[1:], mps[1:]
+
+    # get correct number of planets
+    if Ps.size > nplanets-1:
+        inds = np.arange(Ps.size)
+        np.random.shuffle(inds)
+        Ps, rps, mps = Ps[inds][:nplanets-1], rps[inds][:nplanets-1], \
+                       mps[inds][:nplanets-1]
+    
+    # compute semi-amplitudes
+    Ks = rvs.RV_K(Ps, Ms, mps)
+            
+    return Ps, rps, mps, Ks
+
+
+def draw_M_planets(P, rp, nplanets, Ms):
+    '''
+    Draw planets from around M stars from the occurrence rates derived in 
+    Dressing & Charbonneau 2015. Ensure that each pair in the multi-planet system 
+    is Lagrange stable given circular orbits.
+    '''
+    assert nplanets > 1
+    rpgrid = np.linspace(.5, 4, 8)
+    Pgrid = np.array((.5, 1.7, 5.5, 18.2, 60.3, 200))
+    occurrence = np.array(((1.38, 8.42, 20.59, 0, 0),
+                           (1.95, 9.94, 0, 26.85, 28.85),
+                           (0.41, 4.15, 0, 24.59, 19.98),
+                           (0, 2.72, 18.73, 27.58, 18.08),
+                           (0, 1.59, 8.29, 14.51, 8.61),
+                           (0, 0.65, 3.25, 3.37, 1.97),
+                           (0, 0.38, 1.05, 0.56, 0))).T
+    occurrence *= 1e-2
+    
+    # sample planets until we get the correct multiplicity
+    Ps, mp = np.array([P]), mr.radF2mass(rp)
+    while Ps.size < nplanets:
+
+        Ps, rps = np.array([P]), np.array([rp])
+        mps = mr.radF2mass(rps, Fs=np.repeat(336.5, rps.size))
+        for i in range(Pgrid.size-1):
+            for j in range(rpgrid.size-1):
+                
+                if np.random.rand() <= occurrence[i,j] and Pgrid[i] > P:
+
+                    # draw a system
+                    Ps  = np.append(Ps, np.random.uniform(Pgrid[i], Pgrid[i+1]))
+                    rps = np.append(rps, np.random.uniform(rpgrid[j], rpgrid[j+1]))
+                    mps = mr.radF2mass(rps, Fs=np.repeat(336.5, rps.size))
+
+        sort = np.argsort(Ps)
+        Ps, rps, mps = Ps[sort], rps[sort], mps[sort]
+
+        # get stable pairs
+        if Ps.size >= nplanets:
+            lagrangestable = rvs.is_Lagrangestable(Ps, Ms, mps,
+                                                   np.zeros(Ps.size))
+            if lagrangestable[-1] == 1:
+                lagrangestable = np.append(lagrangestable.astype(bool), True)
+            else:
+                lagrangestable = np.append(lagrangestable.astype(bool), False)
+            Ps, rps, mps = Ps[lagrangestable], rps[lagrangestable], \
+                           mps[lagrangestable]
+
+    # exclude TESS planet
+    Ps, rps, mps = Ps[1:], rps[1:], mps[1:]
+
+    # get correct number of planets
+    if Ps.size > nplanets-1:
+        inds = np.arange(Ps.size)
+        np.random.shuffle(inds)
+        Ps, rps, mps = Ps[inds][:nplanets-1], rps[inds][:nplanets-1], \
+                       mps[inds][:nplanets-1]
+    
     # compute semi-amplitudes
     Ks = rvs.RV_K(Ps, Ms, mps)
             
@@ -61,7 +135,12 @@ def _compute_sigmaRV_planets(Ks):
 
 
 # rvs.is_Lagrangestable(Ps, Ms, mps, eccs)
-def get_sigmaRV_planets(P, rp, Ms, mult):
-    _,_,_,Ks = draw_FGK_planet(P, rp, mult, Ms)
-    return _compute_sigmaRV_planets(Ks)
+def get_sigmaRV_planets(P, rp, Teff, Ms, mult):
+    # FGK star
+    if Teff > 3800:
+        _,_,_,Ks = draw_FGK_planets(P, rp, mult, Ms)
+    # M dwarf
+    else:
+        _,_,_,Ks = draw_M_planets(P, rp, mult, Ms)    
 
+    return _compute_sigmaRV_planets(Ks)
