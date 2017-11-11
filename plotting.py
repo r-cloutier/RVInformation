0e0990b541dbf4fbf1eac570f77bae78e1360a33
Ryan Cloutier <cloutier@astro.utoronto.ca>
2017-11-11 17:34:05 -0500

    updated saved results location and plotting functions

--- plotting.py
+++ plotting.py
@@ -14,28 +14,30 @@ def plot_Nrv_estimates(pltt=True, label=False):
     Nrv_calc = compute_Nrv(sigeffs, sigKs)
 
     # plotting
-    fig = plt.figure()
+    fig = plt.figure(figsize=(6,6))
     ax = fig.add_subplot(111, aspect='equal')
     g = np.isfinite(multflag)#multflag == 1
-    cax = ax.scatter(Nrv_true[g], Nrv_calc[g], edgecolors='none', c=np.log10(sigKs[g]), 
-		     marker='o', s=100)
-    #g = multflag == 0
-    #cax = ax.scatter(Nrv_true[g], Nrv_calc[g], edgecolors='none', c=np.log10(sigeffs[g]),
-    #                 marker='s', s=100)
-    #cbar_axes = fig.add_axes([.1,.9,.87,.04])
-    #cbar = fig.colorbar(cax, cax=cbar_axes, orientation='horizontal')#, pad=.11)
-    #cbar.set_label('log sigK')
+    cax = ax.scatter(Nrv_true[g], Nrv_calc[g], edgecolors='none', c=np.log10(sigeffs[g]),
+                     marker='o', s=100)
+    cbar_axes = fig.add_axes([.1,.9,.87,.04])
+    cbar = fig.colorbar(cax, cax=cbar_axes, orientation='horizontal')#, pad=.11)
+    cbar.set_label('log sigK')
     
     Nrvs = np.append(Nrv_true, Nrv_calc)
     arr = np.linspace(0, 3e2, 100)
     ax.plot(arr, arr, 'k--', lw=2)
-    ax.fill_between(arr, 3e2, arr, alpha=.3)
+    #ax.fill_between(arr, 3e2, arr, alpha=.3)
+    ax.fill_between(arr, .9*arr, 1.1*arr, alpha=.5)
+    ax.fill_between(arr, .8*arr, 1.2*arr, alpha=.5)
+    ax.plot(arr, 1.1*arr, 'k--', lw=.9)
+    ax.plot(arr, .9*arr, 'k--', lw=.9)
 
     ax.set_xscale('log'), ax.set_yscale('log')
     ax.set_xlabel('Actual n$_{RV}$'), ax.set_ylabel('Calculated n$_{RV}$')
-    ax.set_xlim((0, 3e2)), ax.set_ylim((0, 3e2))
+    ax.set_xlim((1, 3e2)), ax.set_ylim((1, 3e2))
     ax.minorticks_on()
-    
+
+    plt.subplots_adjust(bottom=.12)
     if pltt:
         plt.show()
     plt.close('all')
