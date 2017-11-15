6e96580bd526fbe349bcf9252397e5768f13834a
Ryan Cloutier <cloutier@astro.utoronto.ca>
2017-11-15 12:07:43 -0500

    add noisefloor to job submission script

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from runTESS import *
 
 planetindex = int(sys.argv[1])
 band_strs = [['B','V'], ['Y','J','H'], ['Y','J','H','K']]  # HARPS, NIRPS, & SPIRou
-R, aperture_m = [1e5, 1e5, 7e4], [3.6, 3.6, 3.6]
+R, aperture_m, noisefloor = [1e5, 1e5, 7e4], [3.6, 3.6, 3.6], [.5, 1., 1.]
 nsyst_per_TESSstar = 100
 assert nsyst_per_TESSstar > 0
 
@@ -14,4 +14,4 @@ for i in range(len(band_strs)):
         print estimate_Nrv_TESS(planetindex, band_strs[i], R[i], aperture_m[i],
                                 QE=.1, Z=0, sigmaRV_activity=0, sigmaRV_planets=0,
                                 sigmaRV_noisefloor=noisefloor[i], testingseed=testingseed,
-                                testplanet_sigmaKfrac=0, systnum=j, verbose=True)
+                                testplanet_sigmaKfrac=0, systnum=j, verbose=False)
