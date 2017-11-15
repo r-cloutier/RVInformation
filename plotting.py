4d295d4937d5b46f671e643b23ccf004892a7f38
Ryan Cloutier <cloutier@astro.utoronto.ca>
2017-11-15 10:16:58 -0500

    updated job submission script to 1700 planets

--- plotting.py
+++ plotting.py
@@ -45,17 +45,21 @@ def plot_Nrv_estimates(pltt=True, label=False):
     plt.close('all')
 
     
-def plot_Nrv_F(self, pltt=True, label=False):
+def plot_Nrv_F(self, errorbar=False, pltt=True, label=False):
     fig = plt.figure()
     ax = fig.add_subplot(111)
 
     g = self.Teffs_med > 0
 
-    ax.errorbar(self.Fs_med[g], self.Nrvs_med_I[g], self.Nrvs_emed_I[g], fmt='r.',
-                ms=12, capsize=0, elinewidth=.5)
-    ax.errorbar(self.Fs_med[g], self.Nrvs_med_O[g], self.Nrvs_emed_O[g], fmt='b.',
-                ms=12, capsize=0, elinewidth=.5)
-
+    if errorbar:
+        ax.errorbar(self.Fs_med[g], self.Nrvs_med_I[g], self.Nrvs_emed_I[g],
+                    fmt='r.', ms=12, capsize=0, elinewidth=.5)
+        ax.errorbar(self.Fs_med[g], self.Nrvs_med_O[g], self.Nrvs_emed_O[g],
+                    fmt='b.', ms=12, capsize=0, elinewidth=.5)
+    else:
+        ax.plot(self.Fs_med[g], self.Nrvs_med_I[g], 'ro', ms=10, alpha=.5)
+        ax.plot(self.Fs_med[g], self.Nrvs_med_O[g], 'bo', ms=10, alpha=.5)
+        
     ax.set_xscale('log')#, ax.set_yscale('log')
     ax.set_xlim((1e4,1e-1)), ax.set_ylim((1,2e2))
     ax.set_xlabel('Insolation ($S_{\oplus}$)'), ax.set_ylabel('$n_{RV}$')
@@ -65,6 +69,43 @@ def plot_Nrv_F(self, pltt=True, label=False):
     plt.close('all')
 
 
+def plot_Nrv_mag(self, mag='V', errorbar=True, pltt=True, label=False):
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+
+    if mag == 'B':
+        xarr = self.Bmags_med
+    elif mag == 'V':
+        xarr = self.Vmags_med
+    elif mag == 'Y':
+        xarr = self.Ymags_med
+    elif mag == 'J':
+        xarr = self.Jmags_med
+    elif mag == 'H':
+        xarr = self.Hmags_med
+    elif mag == 'K':
+        xarr = self.Kmags_med
+        
+    g = self.Teffs_med > 0
+
+    if errorbar:
+        ax.errorbar(xarr[g], self.Nrvs_med_I[g], self.Nrvs_emed_I[g],
+                    fmt='r.', ms=12, capsize=0, elinewidth=.5)
+        ax.errorbar(xarr[g], self.Nrvs_med_O[g], self.Nrvs_emed_O[g],
+                    fmt='b.', ms=12, capsize=0, elinewidth=.5)
+    else:
+        ax.plot(xarr[g], self.Nrvs_med_I[g], 'ro', ms=10, alpha=.5)
+        ax.plot(xarr[g], self.Nrvs_med_O[g], 'bo', ms=10, alpha=.5)
+        
+    #ax.set_xlim((1e4,1e-1))
+    ax.set_ylim((1,2e2))
+    ax.set_xlabel('$%s$'%mag), ax.set_ylabel('$n_{RV}$')
+    
+    if pltt:
+        plt.show()
+    plt.close('all')
+    
+
 def plot_Nrvratio(self, pltt=True, label=False):
     fig = plt.figure()
     ax = fig.add_subplot(111)
@@ -72,7 +113,8 @@ def plot_Nrvratio(self, pltt=True, label=False):
     ratio = unumpy.uarray(self.Nrvs_med_I, self.Nrvs_emed_I) / \
             unumpy.uarray(self.Nrvs_med_O, self.Nrvs_emed_O)
     
-    ax.errorbar(self.Teffs_med, unumpy.nominal_values(ratio), unumpy.std_devs(ratio),
+    ax.errorbar(self.Teffs_med, unumpy.nominal_values(ratio),
+                unumpy.std_devs(ratio),
                 fmt='k.', ms=12, capsize=0, elinewidth=.5)
     xlim = ax.get_xlim()
     ax.fill_between(list(xlim), np.ones(2), color='k', alpha=.2)
