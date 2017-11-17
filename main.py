aae8e309514cb8fde8f7dd168a5c1c833a4a5d8f
Ryan Cloutier <cloutier@astro.utoronto.ca>
2017-11-17 14:08:53 -0500

    quick commit

--- main.py
+++ main.py
@@ -9,8 +9,7 @@ assert nsyst_per_TESSstar > 0
 for i in range(len(band_strs)):
     for j in range(nsyst_per_TESSstar):
 
-        #testingseed = True if nsyst_per_TESSstar == 1 else False
-	testingseed = False  # TEMP
+        testingseed = True if nsyst_per_TESSstar == 1 else False
 
         estimate_Nrv_TESS(planetindex, band_strs[i], R[i], aperture_m[i],
                     	  QE=.1, Z=0, sigmaRV_activity=0, sigmaRV_planets=0,
