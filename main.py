349614ff9018c3ff57d48408782237d2e42461df
Ryan Cloutier <cloutier@astro.utoronto.ca>
2017-11-26 19:48:55 -0500

    quick commit

--- main.py
+++ main.py
@@ -13,20 +13,20 @@ if __name__ == '__main__':
     # HARPS, NIRPS, & SPIRou
     band_strs = [['B','V'], ['Y','J','H'], ['Y','J','H','K']]
     R, aperture_m, noisefloor = [1e5, 1e5, 7e4], [3.6, 3.6, 3.6], [.5, 1., 1.]
-    nsyst_per_TESSstar = 1
+    nsyst_per_TESSstar = 100
     assert nsyst_per_TESSstar > 0
 
-    for i in range(len(band_strs)):
-        for j in range(nsyst_per_TESSstar):
+    for i in range(nsyst_per_TESSstar):
+        for j in range(len(band_strs)):
 
             testingseed = True if nsyst_per_TESSstar == 1 else False
             
-            if do_i_run_this_job(planetindex, band_strs[i], j):
+            if do_i_run_this_job(planetindex, band_strs[j], i):
 
-                estimate_Nrv_TESS(planetindex, band_strs[i], R[i],
-                                  aperture_m[i], QE=.1, Z=0, sigmaRV_activity=0,
+                estimate_Nrv_TESS(planetindex, band_strs[j], R[j],
+                                  aperture_m[j], QE=.1, Z=0, sigmaRV_activity=0,
                                   sigmaRV_planets=0,
-                                  sigmaRV_noisefloor=noisefloor[i],
+                                  sigmaRV_noisefloor=noisefloor[j],
                                   testingseed=testingseed,
-                                  testplanet_sigmaKfrac=0, systnum=j,
+                                  testplanet_sigmaKfrac=0, systnum=i,
                                   verbose=False)
