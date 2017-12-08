10d28d36195ed79cafde3337bc0aff4f43efa7ff
Ryan Cloutier <cloutier@astro.utoronto.ca>
2017-12-08 11:51:18 -0500

    about to mess with cumulative planet detection plot

--- plotting.py
+++ plotting.py
@@ -2,7 +2,9 @@ import numpy as np
 import pylab as plt
 from uncertainties import unumpy as unp
 
-global rpedges, rplabels
+global Teffedges, Tefflabels, rpedges, rplabels
+Teffedges = [25e2, 32e2, 38e2, 76e2, 12e3]
+Tefflabels = ['mid-late M', 'early-mid M', 'FGK', 'BA']
 rpedges = np.array([0, 1.25, 2, 4, 30])
 rplabels = ['Earths','Super-Earths','Sub-Neptunes','Giants']
 
@@ -17,58 +19,77 @@ def plot_Nrv_hists(self, nbins=40, pltt=True, label=False):
     opt, nir = self.spectrographs == 'H', self.spectrographs == 'N'
     fig = plt.figure()
     ax = fig.add_subplot(111)
-    ax.hist(self.Nrvs[opt], bins=bins, color='b', alpha=.6, label='Optical')
-    ax.hist(self.Nrvs[nir], bins=bins, color='r', alpha=.6, label='nIR')
+    ax.hist(self.Nrvs[opt], bins=bins, color='b', alpha=.6, normed=0,
+            stacked=1, label='Optical')
+    ax.hist(self.Nrvs[nir], bins=bins, color='r', alpha=.6, normed=0,
+            stacked=1, label='nIR')
     ax.set_xscale('log')
+    ax.set_xlabel('$n_{RV}$')#, ax.set_ylabel('
 
     if pltt:
 	plt.show()
     plt.close('all')
 
 
-def plot_Nrv_hists_v_Teff(self, nbins=40, pltt=True, label=False):
+def plot_tobs_hists_v_Teff(self, nbins=40, pltt=True, label=False):
     bins = np.logspace(0,5,nbins)
-    Teffedges = [25e2, 32e2, 38e2, 76e2, 12e3]
     opt, nir = self.spectrographs == 'H', self.spectrographs == 'N'
-    fig = plt.figure(figsize=(12,4))
+    fig = plt.figure(figsize=(12,3.4))
     for i in range(len(Teffedges)-1): 
-        ax = fig.add_subplot(1,len(Teffedges)-1,i+1)
+        ax = fig.add_subplot(1,len(Teffedges)-1,-i+4)
 	gteff = (self.Teffs >= Teffedges[i]) & (self.Teffs <= Teffedges[i+1])
     	opt = (self.spectrographs == 'H') & gteff
-	ax.hist(self.Nrvs[opt], bins=bins, color='b', alpha=.6, label='Optical')
-	ax.text(.65, .85, 'med(Nrv)=%.1f'%(np.median(self.Nrvs[opt])), color='b', transform=ax.transAxes)
+	ax.hist(self.tobss[opt], bins=bins, color='b', alpha=.6,
+                label='Optical')
+	ax.text(.68, .85, 'med(tobs)=%.1f'%(np.median(self.tobss[opt])),
+                color='b', transform=ax.transAxes, fontsize=10)
 	nir = (self.spectrographs == 'N') & gteff
-        ax.hist(self.Nrvs[nir], bins=bins, color='r', alpha=.6, label='nIR')
-        ax.text(.65, .8, 'med(Nrv)=%.1f'%(np.median(self.Nrvs[nir])), color='r', transform=ax.transAxes)
-    	ax.set_xscale('log'), ax.set_xlabel('Nrv')
+        ax.hist(self.tobss[nir], bins=bins, color='r', alpha=.6,
+                label='nIR')
+        ax.text(.68, .8, 'med(tobs)=%.1f'%(np.median(self.tobss[nir])),
+                color='r', transform=ax.transAxes, fontsize=10)
+    	ax.set_xscale('log'), ax.set_xlabel('$t_{obs}$ [hours]', fontsize=10)
 	ax.set_yticklabels('')
-	ax.set_title(r'%i $\leq T_{eff} <$ %i'%(Teffedges[i], Teffedges[i+1]))
+	ax.set_title('%s\n%i $\leq T_{eff} <$ %i'%(Tefflabels[i],
+                                                   Teffedges[i],
+                                                   Teffedges[i+1]),
+                     fontsize=10)
+        ax.minorticks_on()
 
-    fig.subplots_adjust(left=.07, right=.96)
+    fig.subplots_adjust(left=.04, bottom=.17, top=.87, right=.96)
+    if label:
+        plt.savefig('plots/tobshist_Teff.png')
     if pltt:
         plt.show()
     plt.close('all')
 
 
-def plot_Nrv_hists_v_rp(self, nbins=40, pltt=True, label=False):
+def plot_tobs_hists_v_rp(self, nbins=40, pltt=True, label=False):
     bins = np.logspace(0,5,nbins)
-    rplabels = ['$<1.25 R_{\oplus}$','$1.25-2 R_{oplus}$','$2-4 R_{\oplus}$','$>4 R_{\oplus}$']
+    rplabels2 = ['$<1.25 R_{\oplus}$','$1.25-2 R_{\oplus}$',
+                 '$2-4 R_{\oplus}$','$>4 R_{\oplus}$']
     opt, nir = self.spectrographs == 'H', self.spectrographs == 'N'
-    fig = plt.figure(figsize=(12,4))
+    fig = plt.figure(figsize=(12,3.4))
     for i in range(len(rpedges)-1):
         ax = fig.add_subplot(1,len(rpedges)-1,i+1)
         grp = (self.rps >= rpedges[i]) & (self.rps <= rpedges[i+1])
         opt = (self.spectrographs == 'H') & grp
-        ax.hist(self.Nrvs[opt], bins=bins, color='b', alpha=.6, label='Optical')
-        ax.text(.65, .85, 'med(Nrv)=%.1f'%(np.median(self.Nrvs[opt])), color='b', transform=ax.transAxes)
+        ax.hist(self.tobss[opt], bins=bins, color='b', alpha=.6,
+                label='Optical')
+        ax.text(.65, .85, 'med(tobs)=%.1f'%(np.median(self.tobss[opt])),
+                color='b', transform=ax.transAxes, fontsize=10)
         nir = (self.spectrographs == 'N') & grp
-        ax.hist(self.Nrvs[nir], bins=bins, color='r', alpha=.6, label='nIR')
-        ax.text(.65, .8, 'med(Nrv)=%.1f'%(np.median(self.Nrvs[nir])), color='r', transform=ax.transAxes)
-        ax.set_xscale('log'), ax.set_xlabel('Nrv')
+        ax.hist(self.tobss[nir], bins=bins, color='r', alpha=.6,
+                label='nIR')
+        ax.text(.65, .8, 'med(tobs)=%.1f'%(np.median(self.tobss[nir])),
+                color='r', transform=ax.transAxes, fontsize=10)
+        ax.set_xscale('log'), ax.set_xlabel('$t_{obs}$ [hours]', fontsize=10)
         ax.set_yticklabels('')
-        ax.set_title('%s'%rplabels[i])
+        ax.set_title('%s\n%s'%(rplabels[i], rplabels2[i]), fontsize=10)
 
-    fig.subplots_adjust(left=.07, right=.96)
+    fig.subplots_adjust(left=.04, bottom=.17, top=.87, right=.96)
+    if label:
+        plt.savefig('plots/tobshist_rp.png')
     if pltt:
         plt.show()
     plt.close('all')
@@ -114,25 +135,34 @@ def plot_Nrv_estimates(pltt=True, label=False):
     plt.close('all')
 
     
-def plot_Nrv_F(self, errorbar=False, pltt=True, label=False):
+def plot_tobs_F(self, errorbar=False, pltt=True, label=False):
     fig = plt.figure()
     ax = fig.add_subplot(111)
 
     g = self.Teffs_med > 0
 
     if errorbar:
-        ax.errorbar(self.Fs_med[g], self.Nrvs_med_I[g], self.Nrvs_emed_S[g],
+        ax.errorbar(self.Fs_med[g], self.tobss_med_I[g], self.tobss_emed_S[g],
                     fmt='ro', ms=3, capsize=0, elinewidth=.4, alpha=.6)
-        ax.errorbar(self.Fs_med[g], self.Nrvs_med_H[g], self.Nrvs_emed_H[g],
+        ax.errorbar(self.Fs_med[g], self.tobss_med_H[g], self.tobss_emed_H[g],
                     fmt='bo', ms=3, capsize=0, elinewidth=.4, alpha=.6)
     else:  # point estimates
-        ax.plot(self.Fs_med[g], self.Nrvs_med_S[g], 'ro', ms=8, alpha=.5)
-        ax.plot(self.Fs_med[g], self.Nrvs_med_H[g], 'bo', ms=8, alpha=.5)
+        ax.scatter(self.Fs_med[g], self.tobss_med_S[g], edgecolor='r',
+                   s=self.Ks_med[g]*2,
+                   alpha=1, facecolors='none', marker='d', label='nIR')
+        ax.scatter(self.Fs_med[g], self.tobss_med_H[g], edgecolor='b',
+                   s=2*self.mps_med[g],
+                   alpha=1, facecolors='none', marker='o', label='Optical')
         
     ax.set_xscale('log'), ax.set_yscale('log')
-    ax.set_xlim((1e4,1e-1)), ax.set_ylim((1,1e3))
-    ax.set_xlabel('Insolation ($S_{\oplus}$)'), ax.set_ylabel('$n_{RV}$')
-    
+    ax.set_xlim((1e4,6e-2)), ax.set_ylim((1,1e4))
+    ax.set_xlabel('Insolation ($S_{\oplus}$)')
+    ax.set_ylabel('$t_{obs}$ [hours]')
+    ax.legend(bbox_to_anchor=(.25,.96))
+
+    fig.subplots_adjust(bottom=.14, top=.91)
+    if label:
+        plt.savefig('plots/tobs_F.png')
     if pltt:
         plt.show()
     plt.close('all')
@@ -236,27 +266,32 @@ def plot_NIRPSvSPIROU(self, pltt=True, label=False):
     plt.close('all')
 
 
-def plot_cumulative_detections_v_tobs(self, pltt=True, label=False, harps=True, nirps=True, tmax=1e4):
-    tarr = np.arange(0,1e3,1)
+def plot_cumulative_detections_v_tobs(self, pltt=True, label=False,
+                                      harps=True, nirps=True, tmax=1e4):
+    tarr = np.arange(0,tmax,1)
     fig = plt.figure(figsize=(12,3))
 
-    for i in range(rpedges.size-1):
-	ax = fig.add_subplot(1, rpedges.size-1, i+1)
+    for i in range(Teffedges.size-1):
+	ax = fig.add_subplot(1, Teffedges.size-1, i+1)
 
 	# HARPS total observing time
 	if harps:
-            g = (self.rps_med >= rpedges[i]) & (self.rps_med <= rpedges[i+1])
-	    tobs = np.cumsum(np.sort(self.tobss_med_H[g]))
-	    Ndet = np.arange(1,tobs.size+1)
-	    g = tobs <= tmax
-	    ax.plot(tobs[g], Ndet[g], 'b-', drawstyle='steps')
-	    if i == 0:
-		ax.text(.1, .85, 'HARPS-like', color='b', fontsize=10, 
-			weight='normal', transform=ax.transAxes)
+            for j in range(rpedges.size-1):
+                g = (self.Teffs_med >= Teffedges[i]) & \
+                    (self.Teffs_med < Teffedges[i+1]) & \
+                    (self.rps_med >= rpedges[j]) & \
+                    (self.rps_med < rpedges[j+1])
+	        tobs = np.cumsum(np.sort(self.tobss_med_H[g]))
+	        Ndet = np.arange(1,tobs.size+1)
+	        g = tobs <= tmax
+	        ax.plot(tobs[g], Ndet[g], 'b-', drawstyle='steps')
+	        if i == 0:
+		    ax.text(.1, .85, 'HARPS-like', color='b', fontsize=10, 
+			    weight='normal', transform=ax.transAxes)
  
         # NIRPS total observing time
 	if nirps:
-            g = (self.rps_med >= rpedges[i]) & (self.rps_med <= rpedges[i+1])
+            g = (self.rps_med >= rpedges[i]) & (self.rps_med < rpedges[i+1])
             tobs = np.cumsum(np.sort(self.tobss_med_S[g]))  # TEMP
             Ndet = np.arange(1,tobs.size+1)
 	    g = tobs <= tmax
@@ -265,7 +300,8 @@ def plot_cumulative_detections_v_tobs(self, pltt=True, label=False, harps=True, 
                 ax.text(.1, .75, 'NIRPS-like', color='r', fontsize=11, 
                         weight='normal', transform=ax.transAxes)
 
-	ax.set_title(rplabels[i]), ax.set_xlabel('Total observing time [hours]', fontsize=9)
+	ax.set_title(rplabels[i])
+        ax.set_xlabel('Total observing time [hours]', fontsize=9)
 	if i == 0:
 	    ax.set_ylabel('Cumulative number of\nplanet detections')	
 	ax.minorticks_on()
